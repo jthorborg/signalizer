e62c35e81a82d28b2e6288b43cd3f5ed32bbc1f9
Mayae <janus.thorborg@gmail.com>
2016-04-20 16:03:58 +0200

    Bumped up version
    Former-commit-id: 573bb60c88d4afe9e73d31f0844edf9ff9b69425

--- Source/version.h
+++ Source/version.h
@@ -1,4 +1,4 @@
 #define SIGNALIZER_MAJOR 0
 #define SIGNALIZER_MINOR 2
-#define SIGNALIZER_BUILD 5
-#define SIGNALIZER_BUILD_INFO "* dev\n  master\n\nc920aa5\n"
+#define SIGNALIZER_BUILD 6
+#define SIGNALIZER_BUILD_INFO "* dev\n  master\n\n2d1723b\n"
