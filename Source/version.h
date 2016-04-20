3436409ee94b0a7dc541bbe9748790f176380e6c
Mayae <janus.thorborg@gmail.com>
2016-04-21 01:22:33 +0200

    Public alpha release 0.2.6: As stable as 0.2.5, but not there yet. New in this version: Better debug output.
    Former-commit-id: 9c82f28ded4d8512a5f427c4097b74809802fdda

--- Source/version.h
+++ Source/version.h
@@ -1,4 +1,4 @@
 #define SIGNALIZER_MAJOR 0
 #define SIGNALIZER_MINOR 2
 #define SIGNALIZER_BUILD 6
-#define SIGNALIZER_BUILD_INFO "* dev\n  master\n\n573bb60\n"
+#define SIGNALIZER_BUILD_INFO "* dev\n  master\n\nbc66f0a\n"
