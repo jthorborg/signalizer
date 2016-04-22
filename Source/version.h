0f328a1d9a1826d6eb93b8fe76a2dae884ecd590
Mayae <janus.thorborg@gmail.com>
2016-04-22 17:20:25 +0200

    Windows XP support.
    Former-commit-id: d6daad51db25d55997fbb6cf07277a80f617c5da

--- Source/version.h
+++ Source/version.h
@@ -1,4 +1,4 @@
 #define SIGNALIZER_MAJOR 0
 #define SIGNALIZER_MINOR 2
-#define SIGNALIZER_BUILD 6
-#define SIGNALIZER_BUILD_INFO "* dev\n  master\n\nbc66f0a\n"
+#define SIGNALIZER_BUILD 7
+#define SIGNALIZER_BUILD_INFO "* dev\n  master\n\n45080d2\n"
