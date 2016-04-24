b882442c7686d6567364bbffd1d221539aa4e311
Mayae <janus.thorborg@gmail.com>
2016-04-25 01:11:38 +0200

    0.2.7 binaries for Windows.
    Former-commit-id: c60338ccb7defe1312a6e73142a759b7ca5853e5

--- Source/version.h
+++ Source/version.h
@@ -1,7 +1,7 @@
 #define SIGNALIZER_MAJOR 0
 #define SIGNALIZER_MINOR 2
 #define SIGNALIZER_BUILD 7
-#define SIGNALIZER_BUILD_INFO "* dev\n  master\n\nf3ce526\n"
+#define SIGNALIZER_BUILD_INFO "* dev\n  master\n\n06b82a0\n"
 
 #define SIGNALIZER_VERSION_STRING "0.2.7"
 #define SIGNALIZER_VST_VERSION_HEX 0x000207
