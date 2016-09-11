d10e4913ab9be61c31750cfc56620055854d8d9c
Mayae <janus.thorborg@gmail.com>
2016-09-11 17:48:43 +0200

    Windows version updates
    Former-commit-id: 3f537aeb6bafad035722b056b5e1707a84791d0f

--- Source/version.h
+++ Source/version.h
@@ -1,7 +1,7 @@
 #define SIGNALIZER_MAJOR 0
 #define SIGNALIZER_MINOR 2
 #define SIGNALIZER_BUILD 10
-#define SIGNALIZER_BUILD_INFO "* dev\n  master\n\n4b0d643\n"
+#define SIGNALIZER_BUILD_INFO "* dev\n  master\n\n8a0180c\n"
 
 #define SIGNALIZER_VERSION_STRING "0.2.10"
 #define SIGNALIZER_VST_VERSION_HEX 0x00020a
