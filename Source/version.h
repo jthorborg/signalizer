be05fdf5b50b4cd7af79b937ac4b4861a670914f
Janus Lynggaard Thorborg <janus.thorborg@gmail.com>
2017-03-23 01:23:42 +0100

    Bumped dev version to 0.3.0, seeing as the feature complete oscilloscope is now included.
    Former-commit-id: 92b450f5e1122d5a430b5bc56cc2232c88d1fe16

--- Source/version.h
+++ Source/version.h
@@ -1,7 +1,7 @@
 #define SIGNALIZER_MAJOR 0
-#define SIGNALIZER_MINOR 2
-#define SIGNALIZER_BUILD 10
-#define SIGNALIZER_BUILD_INFO "  dev\n  linux-support\n  master\n  osc/feature-colours\n  osc/triggers\n* oscilloscope\n\n7d7e596\n"
+#define SIGNALIZER_MINOR 3
+#define SIGNALIZER_BUILD 0
+#define SIGNALIZER_BUILD_INFO "* dev\n  linux-support\n  master\n  osc/feature-colours\n  osc/triggers\n  oscilloscope\n\n016aad3\n"
 
-#define SIGNALIZER_VERSION_STRING "0.2.10"
-#define SIGNALIZER_VST_VERSION_HEX 0x00020a
+#define SIGNALIZER_VERSION_STRING "0.3.0"
+#define SIGNALIZER_VST_VERSION_HEX 0x000300
