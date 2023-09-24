099e18c7c2f1da6d7d3541ef1792eff77cf6db8a
Janus Thorborg <janus.thorborg@gmail.com>
2023-09-24 14:29:18 +0200

    Updated build to 0.3.8

--- Source/version.h
+++ Source/version.h
@@ -1,7 +1,7 @@
 #define SIGNALIZER_MAJOR 0
 #define SIGNALIZER_MINOR 3
-#define SIGNALIZER_BUILD 7
-#define SIGNALIZER_BUILD_INFO "* dev\n  feature/multi-channel\n  feature/multi-channel-macos\n  master\n  refactor/dependencies\n\ne8d0551\n"
+#define SIGNALIZER_BUILD 8
+#define SIGNALIZER_BUILD_INFO "* dev\n  embed/unity\n  feature/multi-channel\n  feature/multi-channel-double\n  feature/multi-channel-macos\n  feature/multi-channel-spectrum-upgrade\n  feature/multi-channel-wip\n  master\n  osc/peak-triggers\n  refactor/dependencies\n\nf8c22dc\n"
 
-#define SIGNALIZER_VERSION_STRING "0.3.7"
-#define SIGNALIZER_VST_VERSION_HEX 0x000307
+#define SIGNALIZER_VERSION_STRING "0.3.8"
+#define SIGNALIZER_VST_VERSION_HEX 0x000308
