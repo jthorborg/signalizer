4e57498133458eb397eab47206f7e58e90cc7d09
Janus Thorborg <janus.thorborg@gmail.com>
2023-11-19 23:54:12 +0100

    Increasing version to 0.4.0 / Windows

--- Source/version.h
+++ Source/version.h
@@ -1,7 +1,7 @@
 #define SIGNALIZER_MAJOR 0
-#define SIGNALIZER_MINOR 3
-#define SIGNALIZER_BUILD 8
-#define SIGNALIZER_BUILD_INFO "* dev\n  embed/unity\n  feature/multi-channel\n  feature/multi-channel-double\n  feature/multi-channel-macos\n  feature/multi-channel-spectrum-upgrade\n  feature/multi-channel-wip\n  master\n  osc/peak-triggers\n  refactor/dependencies\n\nf8c22dc\n"
+#define SIGNALIZER_MINOR 4
+#define SIGNALIZER_BUILD 0
+#define SIGNALIZER_BUILD_INFO "  convert-osc-trigger-to-unifft\n  dev\n  embed/unity\n  feature/multi-channel\n  feature/multi-channel-double\n  feature/multi-channel-macos\n  feature/multi-channel-spectrum-upgrade\n  feature/multi-channel-wip\n  master\n  osc/peak-triggers\n  refactor/dependencies\n* release/0.4.0\n\nbefea27\n"
 
-#define SIGNALIZER_VERSION_STRING "0.3.8"
-#define SIGNALIZER_VST_VERSION_HEX 0x000308
+#define SIGNALIZER_VERSION_STRING "0.4.0"
+#define SIGNALIZER_VST_VERSION_HEX 0x000400
