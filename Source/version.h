300b3d9bfd470ca95124a604dfd47968a7ea3f20
Janus Thorborg <janus.thorborg@gmail.com>
2023-11-26 19:38:55 +0100

    Version bump on Windows

--- Source/version.h
+++ Source/version.h
@@ -1,7 +1,7 @@
 #define SIGNALIZER_MAJOR 0
 #define SIGNALIZER_MINOR 4
-#define SIGNALIZER_BUILD 0
-#define SIGNALIZER_BUILD_INFO "  convert-osc-trigger-to-unifft\n  dev\n  embed/unity\n  feature/multi-channel\n  feature/multi-channel-double\n  feature/multi-channel-macos\n  feature/multi-channel-spectrum-upgrade\n  feature/multi-channel-wip\n  master\n  osc/peak-triggers\n  refactor/dependencies\n* release/0.4.0\n\nbefea27\n"
+#define SIGNALIZER_BUILD 1
+#define SIGNALIZER_BUILD_INFO "  convert-osc-trigger-to-unifft\n  dev\n  embed/unity\n  feature/multi-channel\n  feature/multi-channel-double\n  feature/multi-channel-macos\n  feature/multi-channel-spectrum-upgrade\n  feature/multi-channel-wip\n* master\n  osc/peak-triggers\n  refactor/dependencies\n  release/0.3.2\n  release/0.4.0\n\n3e33949\n"
 
-#define SIGNALIZER_VERSION_STRING "0.4.0"
-#define SIGNALIZER_VST_VERSION_HEX 0x000400
+#define SIGNALIZER_VERSION_STRING "0.4.1"
+#define SIGNALIZER_VST_VERSION_HEX 0x000401
