852677957df42114a0fde175c3c41c6e9d5bda42
Janus@w <contact@jthorborg.com>
2023-12-21 01:19:36 +0100

    Updated versions for Xcode

--- Source/version.h
+++ Source/version.h
@@ -1,7 +1,7 @@
 #define SIGNALIZER_MAJOR 0
 #define SIGNALIZER_MINOR 4
 #define SIGNALIZER_BUILD 2
-#define SIGNALIZER_BUILD_INFO "  convert-osc-trigger-to-unifft\n  dev\n  embed/unity\n  feature/multi-channel\n  feature/multi-channel-double\n  feature/multi-channel-macos\n  feature/multi-channel-spectrum-upgrade\n  feature/multi-channel-wip\n* master\n  osc/peak-triggers\n  refactor/dependencies\n  release/0.3.2\n  release/0.4.0\n\n3db280c\n"
+#define SIGNALIZER_BUILD_INFO "  dev\n  feature/multi-channel\n  feature/multi-channel-macos\n* master\n  refactor/dependencies\n  release/0.4.0\n\ne9b1916\n"
 
 #define SIGNALIZER_VERSION_STRING "0.4.2"
 #define SIGNALIZER_VST_VERSION_HEX 0x000402
