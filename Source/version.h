86be84f610e5256f4d72f8aa5b8a9a23336bb4a9
Janus Thorborg <janus.thorborg@gmail.com>
2026-05-17 17:57:06 +0200

    Updated advice for windows installation.

--- Source/version.h
+++ Source/version.h
@@ -1,7 +1,7 @@
 #define SIGNALIZER_MAJOR 0
 #define SIGNALIZER_MINOR 5
 #define SIGNALIZER_BUILD 0
-#define SIGNALIZER_BUILD_INFO "* build/juce-update-8\n  build/juce-update-8-arm-support\n  master\n\n3513747\n"
+#define SIGNALIZER_BUILD_INFO "* build/juce-update-8\n  build/juce-update-8-arm-support\n  build/mt\n  convert-osc-trigger-to-unifft\n+ dev\n  embed/unity\n  feature/multi-channel\n  feature/multi-channel-double\n  feature/multi-channel-macos\n  feature/multi-channel-spectrum-upgrade\n  feature/multi-channel-wip\n  master\n  osc/peak-triggers\n  refactor/dependencies\n  release/0.3.2\n  release/0.4.0\n  release/0.4.x\n\nb60f624\n"
 
 #define SIGNALIZER_VERSION 0.5.0
 #define SIGNALIZER_VERSION_STRING "0.5.0"
